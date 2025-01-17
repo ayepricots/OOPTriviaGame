216be9dd52ba0e7bc1003e783a401286c47000aa
Ivory Huang <thua238@aucklanduni.ac.nz>
2025-01-18 00:32:00 +0800

    feat:Create feedback page with same layout.

--- trivia-game/src/app/feedback/page.tsx
+++ trivia-game/src/app/feedback/page.tsx
@@ -1,10 +1,35 @@
+import Image from "next/image";
+import backgroundImage from "../../assets/bg_dim.png";
+import tankImage from "../../assets/window_tank.png";
+import windowLong from "../../assets/window_long.png";
+
 export default function Feedback() {
 	return (
-		<div>
-			<h1>Feeback</h1>
-			<p>
-				Feedback on the trivia game
-			</p>
+		<div className="relative h-screen w-full flex items-center justify-center">
+			<Image
+				src={backgroundImage}
+				alt="Background"
+				fill
+				quality={100}
+				className="z-[-1] object-cover"
+			/>
+
+			<div className="flex space-x-4">
+				<div className="w-[400px] h-[310px] overflow-hidden relative">
+					<Image src={tankImage} alt="Tank" fill className="object-cover" />
+				</div>
+				<div className="relative flex w-[350px] h-[610px] flex-col justify-between ">
+					<Image
+						src={windowLong}
+						alt="Window Long"
+						className="object-cover"
+						quality={100}
+					/>
+
+					<div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center space-y-4">
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
