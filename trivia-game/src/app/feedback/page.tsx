e7540d64985154af1677fb0e18340a26f5bb4363
Ivory Huang <thua238@aucklanduni.ac.nz>
2025-02-18 20:27:06 +1300

    fix:delete images because renaming them is not working :(

--- trivia-game/src/app/feedback/page.tsx
+++ trivia-game/src/app/feedback/page.tsx
@@ -8,7 +8,7 @@ import tankImage from "../../assets/window_tank.png";
 import windowLong from "../../assets/window_long.png";
 import playImage from "../../assets/btn_play.png";
 import quitImage from "../../assets/btn_quit.png";
-//test: trying to change the .PNG to .png
+//test: trying to change the .PNG to .png - delete then re add next commit
 
 interface Player {
 	id: string;
